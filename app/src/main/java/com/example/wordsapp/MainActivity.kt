cd70694d5eef2d66b6a2df1d7fdaa6288d5b047e
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-11 01:16:12 +0300

    words app commit

--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -16,7 +16,10 @@
 package com.example.wordsapp
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.databinding.ActivityMainBinding
@@ -40,7 +43,28 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = LetterAdapter()
     }
     private fun chooseLayout(){
-        if
+        if (isLinearLayoutManager){
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        }else{
+            recyclerView.layoutManager = GridLayoutManager(this,4)
+        }
+        recyclerView.adapter = LetterAdapter()
+    }
+    private fun setIcon(menuItem: MenuItem?){
+        if (menuItem == null)
+            return
+        // Set the drawable for the menu icon based on which LayoutManager is currently in use
+
+        // An if-clause can be used on the right side of an assignment if all paths return a value.
+        // The following code is equivalent to
+        // if (isLinearLayoutManager)
+        //     menu.icon = ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+        // else menu.icon = ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this,R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this,R.drawable.ic_linear_layout)
+
     }
 
 }
