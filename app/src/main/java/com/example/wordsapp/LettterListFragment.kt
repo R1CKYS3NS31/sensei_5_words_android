178bf5a9271abde830cedd7daa68ef5c27f15886
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-24 22:06:46 +0300

    words app LetterListFragment commit

--- app/src/main/java/com/example/wordsapp/LettterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LettterListFragment.kt
@@ -1,7 +1,30 @@
 package com.example.wordsapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.FragmentLettterListBinding
 
 class LettterListFragment : Fragment() {
+    private var _binding:FragmentLettterListBinding?=null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLettterListBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
 }
