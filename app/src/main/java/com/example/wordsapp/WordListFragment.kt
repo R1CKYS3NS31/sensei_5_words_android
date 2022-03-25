791cbf4e9a3fc5801d55592109765c8bb64385ac
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-26 00:00:06 +0300

    words app WordListFragment commit

--- app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -47,8 +47,8 @@ class WordListFragment : Fragment() {
 
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 
