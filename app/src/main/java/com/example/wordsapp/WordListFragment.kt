b6f412df107b129bd1f6cb88c92258f08ccf7b11
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-25 15:10:54 +0300

    words app WordListFragment commit

--- app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -1,7 +1,27 @@
 package com.example.wordsapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.wordsapp.databinding.FragmentWordListBinding
 
 class WordListFragment : Fragment() {
+    companion object{
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX =  "https://www.google.com/search?q="
+    }
+    var _binding:FragmentWordListBinding?=null
+    val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWordListBinding.inflate(inflater,container,false)
+        return binding?.root
+    }
 
 }
